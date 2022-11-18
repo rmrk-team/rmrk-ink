7ebd95b236af093fe13fd9a833bc91ab34c727d5
Mar.io <34627453+Maar-io@users.noreply.github.com>
2022-11-18 14:38:12 +0100

    moved traits to other file

--- logics/impls/rmrk/nesting.rs
+++ logics/impls/rmrk/nesting.rs
@@ -23,52 +23,11 @@ use ink_prelude::collections::BTreeSet;
 
 use crate::impls::rmrk::errors::RmrkError;
 use crate::impls::rmrk::types::*;
-pub use crate::traits::nesting::Nesting;
+pub use crate::traits::nesting::{Internal, Nesting, NestingEvents};
 use openbrush::{
     contracts::psp34::extensions::enumerable::*,
     traits::{AccountId, Storage, String},
 };
-
-pub trait Internal {
-    /// Check if child is already accepted
-    fn already_accepted(&self, parent_token_id: Id, child_nft: ChildNft) -> Result<(), PSP34Error>;
-
-    /// Check if child is already pending
-    fn already_pending(&self, parent_token_id: Id, child_nft: ChildNft) -> Result<(), PSP34Error>;
-
-    /// Add the child to the list of accepted children
-    fn add_to_accepted(&mut self, caller: AccountId, parent_token_id: Id, child_nft: ChildNft);
-
-    /// Remove the child to the list of accepted children
-    fn remove_accepted(
-        &mut self,
-        parent_token_id: Id,
-        child_nft: ChildNft,
-    ) -> Result<(), PSP34Error>;
-
-    /// Add the child to the list of pending children
-    fn add_to_pending(&mut self, parent_token_id: Id, child_nft: ChildNft);
-
-    /// Remove the child to the list of pending children
-    fn remove_from_pending(
-        &mut self,
-        parent_token_id: Id,
-        child_nft: ChildNft,
-    ) -> Result<(), PSP34Error>;
-
-    /// Check if token is minted. Return the owner
-    fn ensure_exists(&self, id: Id) -> Result<AccountId, PSP34Error>;
-
-    /// Check if caller is the owner of this parent token
-    fn is_caller_parent_owner(
-        &self,
-        caller: AccountId,
-        parent_token_id: Id,
-    ) -> Result<(), PSP34Error>;
-
-    /// Cross contract call to transfer child nft ownership
-    fn transfer_child_ownership(&self, signer: AccountId, to: AccountId) -> Result<(), PSP34Error>;
-}
 
 /// Implement internal helper trait for Nesting
 impl<T> Internal for T
@@ -219,39 +178,6 @@ where
     ) -> Result<(), PSP34Error> {
         todo!()
     }
-}
-
-pub trait NestingEvents {
-    /// Emit AddedChild event
-    fn _emit_added_child_event(
-        &self,
-        from: AccountId,
-        to: Id,
-        child_collection_address: AccountId,
-        child_token_id: Id,
-    );
-    /// Emit ChildAccepted event
-    fn _emit_child_accepted_event(
-        &self,
-        by: AccountId,
-        to: Id,
-        child_collection_address: AccountId,
-        child_token_id: Id,
-    );
-    /// Emit ChildAccepted event
-    fn _emit_child_removed_event(
-        &self,
-        parent: Id,
-        child_collection_address: AccountId,
-        child_token_id: Id,
-    );
-    /// Emit ChildRejected event
-    fn _emit_child_rejected_event(
-        &self,
-        parent: Id,
-        child_collection_address: AccountId,
-        child_token_id: Id,
-    );
 }
 
 impl<T> Nesting for T
