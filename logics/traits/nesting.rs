827bcff296368de1465d9df749dde01048cd66a5
Mar.io <34627453+Maar-io@users.noreply.github.com>
2022-11-21 16:55:23 +0100

    merge conflicts

--- logics/traits/nesting.rs
+++ logics/traits/nesting.rs
@@ -99,81 +99,81 @@ pub trait Internal {
     ) -> Result<(), PSP34Error>;
 }
 
-#[openbrush::trait_definition]
-pub trait NestingEvents {
-    /// Emit AddedChild event
-    fn _emit_added_child_event(
-        &self,
-        from: AccountId,
-        to: Id,
-        child_collection_address: AccountId,
-        child_token_id: Id,
-    );
-    /// Emit ChildAccepted event
-    fn _emit_child_accepted_event(
-        &self,
-        by: AccountId,
-        to: Id,
-        child_collection_address: AccountId,
-        child_token_id: Id,
-    );
-    /// Emit ChildAccepted event
-    fn _emit_child_removed_event(
-        &self,
-        parent: Id,
-        child_collection_address: AccountId,
-        child_token_id: Id,
-    );
-    /// Emit ChildRejected event
-    fn _emit_child_rejected_event(
-        &self,
-        parent: Id,
-        child_collection_address: AccountId,
-        child_token_id: Id,
-    );
-}
+// #[openbrush::trait_definition]
+// pub trait NestingEvents {
+//     /// Emit AddedChild event
+//     fn _emit_added_child_event(
+//         &self,
+//         from: AccountId,
+//         to: Id,
+//         child_collection_address: AccountId,
+//         child_token_id: Id,
+//     );
+//     /// Emit ChildAccepted event
+//     fn _emit_child_accepted_event(
+//         &self,
+//         by: AccountId,
+//         to: Id,
+//         child_collection_address: AccountId,
+//         child_token_id: Id,
+//     );
+//     /// Emit ChildAccepted event
+//     fn _emit_child_removed_event(
+//         &self,
+//         parent: Id,
+//         child_collection_address: AccountId,
+//         child_token_id: Id,
+//     );
+//     /// Emit ChildRejected event
+//     fn _emit_child_rejected_event(
+//         &self,
+//         parent: Id,
+//         child_collection_address: AccountId,
+//         child_token_id: Id,
+//     );
+// }
 
-pub trait Internal {
-    /// Check if child is already accepted
-    fn already_accepted(&self, parent_token_id: Id, child_nft: ChildNft) -> Result<(), PSP34Error>;
+// pub trait Internal {
+//     /// Check if child is already accepted
+//     fn already_accepted(&self, parent_token_id: Id, child_nft: ChildNft) -> Result<(), PSP34Error>;
 
-    /// Check if child is already pending
-    fn already_pending(&self, parent_token_id: Id, child_nft: ChildNft) -> Result<(), PSP34Error>;
+//     /// Check if child is already pending
+//     fn already_pending(&self, parent_token_id: Id, child_nft: ChildNft) -> Result<(), PSP34Error>;
 
-    /// Add the child to the list of accepted children
-    fn add_to_accepted(&mut self, caller: AccountId, parent_token_id: Id, child_nft: ChildNft);
+//     /// Add the child to the list of accepted children
+//     fn add_to_accepted(&mut self, caller: AccountId, parent_token_id: Id, child_nft: ChildNft);
 
-    /// Remove the child to the list of accepted children
-    fn remove_accepted(
-        &mut self,
-        parent_token_id: Id,
-        child_nft: ChildNft,
-    ) -> Result<(), PSP34Error>;
+//     /// Remove the child to the list of accepted children
+//     fn remove_accepted(
+//         &mut self,
+//         parent_token_id: Id,
+//         child_nft: ChildNft,
+//     ) -> Result<(), PSP34Error>;
 
-    /// Add the child to the list of pending children
-    fn add_to_pending(&mut self, parent_token_id: Id, child_nft: ChildNft);
+//     /// Add the child to the list of pending children
+//     fn add_to_pending(&mut self, parent_token_id: Id, child_nft: ChildNft);
 
-    /// Remove the child to the list of pending children
-    fn remove_from_pending(
-        &mut self,
-        parent_token_id: Id,
-        child_nft: ChildNft,
-    ) -> Result<(), PSP34Error>;
+//     /// Remove the child to the list of pending children
+//     fn remove_from_pending(
+//         &mut self,
+//         parent_token_id: Id,
+//         child_nft: ChildNft,
+//     ) -> Result<(), PSP34Error>;
 
-    /// Check if token is minted. Return the owner
-    fn ensure_exists(&self, id: Id) -> Result<AccountId, PSP34Error>;
+//     /// Check if token is minted. Return the owner
+//     fn ensure_exists(&self, id: Id) -> Result<AccountId, PSP34Error>;
 
-    /// Check if caller is the owner of this parent token
-    fn is_caller_parent_owner(
-        &self,
-        caller: AccountId,
-        parent_token_id: Id,
-    ) -> Result<(), PSP34Error>;
+//     /// Check if caller is the owner of this parent token
+//     fn is_caller_parent_owner(
+//         &self,
+//         caller: AccountId,
+//         parent_token_id: Id,
+//     ) -> Result<(), PSP34Error>;
 
-    /// Cross contract call to transfer child nft ownership
-    fn transfer_child_ownership(
-        &self,
-        to: AccountId,
-        child_nft: ChildNft,
-    ) -> Result<(), PSP34Error>;
-}
+//     /// Cross contract call to transfer child nft ownership
+//     fn transfer_child_ownership(
+//         &self,
+//         to: AccountId,
+//         child_nft: ChildNft,
+//     ) -> Result<(), PSP34Error>;
+// }
