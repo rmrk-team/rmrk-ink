f8673adf0fa70c4c41f151de514a05dba4995fd4
Sergej Sakac <sakacszergej@gmail.com>
2023-04-13 16:02:24 +0200

    query

--- tests/nesting.spec.ts
+++ tests/nesting.spec.ts
@@ -136,6 +136,9 @@ describe("RMRK Nesting tests", () => {
     // bob accepts child
     await acceptChild(child, parent, bob);
 
+    const owner = await child.query.getParentOfChild([child.address, { u64: 1 }]);
+    console.log(owner);
+
     // bob fails to accept already accepted child
     const failAcceptResult = await parent
       .withSigner(bob)
